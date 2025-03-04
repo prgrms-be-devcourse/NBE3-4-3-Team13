7e720195113adad5aa4729064f884b938e196d88
vdvhk12 <vdvhk12@gmail.com>
2025-03-04 22:53:32 +0900

    refactor: ChatRoomResponseMessage 코틀린으로 변환

--- backend/src/main/java/com/app/backend/domain/chat/room/dto/response/ChatRoomResponseMessage.kt
+++ backend/src/main/java/com/app/backend/domain/chat/room/dto/response/ChatRoomResponseMessage.kt
@@ -1,13 +1,6 @@
-package com.app.backend.domain.chat.room.dto.response;
+package com.app.backend.domain.chat.room.dto.response
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public enum ChatRoomResponseMessage {
+enum class ChatRoomResponseMessage(val message: String) {
 	READ_CHAT_ROOMS_SUCCESS("채팅방 목록 조회 성공"),
-	READ_CHAT_ROOM_SUCCESS("채팅방 상세 조회 성공");
-
-	private final String message;
+	READ_CHAT_ROOM_SUCCESS("채팅방 상세 조회 성공")
 }
