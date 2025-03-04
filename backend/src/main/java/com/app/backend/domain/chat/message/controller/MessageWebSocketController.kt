49604ec73ec90fd18566e3dfe2e05a7b747fdbcc
vdvhk12 <vdvhk12@gmail.com>
2025-03-04 22:12:52 +0900

    refactor: MessageWebSocketController 코틀린으로 변환

--- backend/src/main/java/com/app/backend/domain/chat/message/controller/MessageWebSocketController.kt
+++ backend/src/main/java/com/app/backend/domain/chat/message/controller/MessageWebSocketController.kt
@@ -1,29 +1,20 @@
-package com.app.backend.domain.chat.message.controller;
+package com.app.backend.domain.chat.message.controller
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Controller;
-
-import com.app.backend.domain.chat.message.dto.request.MessageRequest;
-import com.app.backend.domain.chat.message.dto.response.MessageResponse;
-import com.app.backend.domain.chat.message.service.MessageService;
-import com.app.backend.global.rabbitmq.ChatMessageProducer;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Controller
+import com.app.backend.domain.chat.message.dto.request.MessageRequest
+import com.app.backend.domain.chat.message.service.MessageService
+import com.app.backend.global.rabbitmq.ChatMessageProducer
 
 @Controller
-@RequiredArgsConstructor
-@Slf4j
-public class MessageWebSocketController {
-
-	private final MessageService messageService;
-	private final ChatMessageProducer chatMessageProducer;
-
+class MessageWebSocketController(
+	private val messageService: MessageService,
+	private val chatMessageProducer: ChatMessageProducer
+) {
 	@MessageMapping("chat.{chatRoomId}")
-	public void sendMessage(@Payload MessageRequest messageRequest) {
-		// 메세지 저장
-		MessageResponse messageResponse = messageService.saveMessage(messageRequest);
-		chatMessageProducer.sendMessage(messageResponse, "chatroom." + messageRequest.getChatRoomId());
+	fun sendMessage(@Payload messageRequest: MessageRequest) {
+		val messageResponse = messageService.saveMessage(messageRequest)
+		chatMessageProducer.sendMessage(messageResponse, "chatroom." + messageRequest.chatRoomId)
 	}
 }
