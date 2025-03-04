59a7d2a7f5dd3fcd4bd30c0ee0d22cde478d84ca
vdvhk12 <vdvhk12@gmail.com>
2025-03-04 22:16:31 +0900

    refactor: MessageController 코틀린으로 변환

--- backend/src/main/java/com/app/backend/domain/chat/message/controller/MessageController.kt
+++ backend/src/main/java/com/app/backend/domain/chat/message/controller/MessageController.kt
@@ -1,40 +1,36 @@
-package com.app.backend.domain.chat.message.controller;
+package com.app.backend.domain.chat.message.controller
 
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.app.backend.domain.chat.message.dto.response.MessageResponse;
-import com.app.backend.domain.chat.message.dto.response.ResponseMessage;
-import com.app.backend.domain.chat.message.service.MessageService;
-import com.app.backend.global.annotation.CustomPageJsonSerializer;
-import com.app.backend.global.dto.response.ApiResponse;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import com.app.backend.domain.chat.message.dto.response.MessageResponse
+import com.app.backend.domain.chat.message.dto.response.ResponseMessage
+import com.app.backend.domain.chat.message.service.MessageService
+import com.app.backend.global.annotation.CustomPageJsonSerializer
+import com.app.backend.global.dto.response.ApiResponse
 
 @RestController
 @RequestMapping("/api/v1/chatrooms")
-@RequiredArgsConstructor
-public class MessageController {
-
-	private final MessageService messageService;
-
+class MessageController(
+	private val messageService: MessageService
+) {
 	@GetMapping("/{id}/messages")
-	@CustomPageJsonSerializer(hasContent = false,
-							  numberOfElements = false,
-							  size = false,
-							  number = false,
-							  hasPrevious = false,
-							  isFirst = false,
-							  sort = false,
-							  empty = false)
-	public ApiResponse<Page<MessageResponse>> getMessagesByChatRoomId(@PathVariable Long id,
-		@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size) {
-		Page<MessageResponse> messages = messageService.getMessagesByChatRoomId(id, page, size);
-		return ApiResponse.of(true, HttpStatus.OK, ResponseMessage.READ_CHAT_MESSAGES_SUCCESS.getMessage(), messages);
+	@CustomPageJsonSerializer(
+		hasContent = false,
+		numberOfElements = false,
+		size = false,
+		number = false,
+		hasPrevious = false,
+		isFirst = false,
+		sort = false,
+		empty = false
+	)
+	fun getMessagesByChatRoomId(
+		@PathVariable id: Long,
+		@RequestParam(defaultValue = "0") page: Int,
+		@RequestParam(defaultValue = "20") size: Int
+	): ApiResponse<Page<MessageResponse>> {
+		val messages = messageService.getMessagesByChatRoomId(id, page, size)
+		return ApiResponse.of(true, HttpStatus.OK, ResponseMessage.READ_CHAT_MESSAGES_SUCCESS.message, messages)
 	}
 }
