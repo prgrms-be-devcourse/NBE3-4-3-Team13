df059ea858a82d688ba27321a0eba6d08ca9a802
vdvhk12 <vdvhk12@gmail.com>
2025-03-05 14:50:36 +0900

    refactor: ChatMessageProducer.java 코틀린으로 전환

--- backend/src/main/java/com/app/backend/global/rabbitmq/ChatMessageProducer.kt
+++ backend/src/main/java/com/app/backend/global/rabbitmq/ChatMessageProducer.kt
@@ -1,26 +1,20 @@
-package com.app.backend.global.rabbitmq;
+package com.app.backend.global.rabbitmq
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import com.app.backend.domain.chat.message.dto.response.MessageResponse;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import com.app.backend.domain.chat.message.dto.response.MessageResponse
+import io.github.oshai.kotlinlogging.KotlinLogging
 
 @Service
-@RequiredArgsConstructor
-@Slf4j
-public class ChatMessageProducer {
+class ChatMessageProducer(
+	private val rabbitTemplate: RabbitTemplate,
+	@Value("\${rabbitmq.exchange.name}") private val exchange: String
+) {
+	private val log = KotlinLogging.logger {}
 
-	private final RabbitTemplate rabbitTemplate;
-
-	@Value("${rabbitmq.exchange.name}")
-	private String exchange;
-
-	public void sendMessage(MessageResponse message, String roomId) {
-		log.info("message send : {}", message);
-		rabbitTemplate.convertAndSend(exchange, roomId, message);
+	fun sendMessage(message: MessageResponse, roomId: String) {
+		log.info { "message send : $message" }
+		rabbitTemplate.convertAndSend(exchange, roomId, message)
 	}
 }
