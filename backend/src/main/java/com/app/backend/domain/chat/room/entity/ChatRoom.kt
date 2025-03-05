6c44ffc9f478e67f7bfc266410b3fecc09094d18
vdvhk12 <vdvhk12@gmail.com>
2025-03-05 11:11:31 +0900

    refactor: ChatRoom 엔티티 코틀린으로 변환 (#11)
    - BaseEntity disabled public으로 수정
    - DataInit에서 ChatRoom 객체 생성자로 생성하도록 변경
    - GroupService에서 ChatRoom 객체 생성자로 생성하도록 변경
    - TestDetailUtil 에서 ChatRoom 객체 생성자로 생성하도록 변경

--- backend/src/main/java/com/app/backend/domain/chat/room/entity/ChatRoom.kt
+++ backend/src/main/java/com/app/backend/domain/chat/room/entity/ChatRoom.kt
@@ -1,52 +1,37 @@
-package com.app.backend.domain.chat.room.entity;
+package com.app.backend.domain.chat.room.entity
 
-import com.app.backend.domain.group.entity.Group;
-import com.app.backend.global.entity.BaseEntity;
+import com.app.backend.domain.group.entity.Group
+import com.app.backend.global.entity.BaseEntity
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "tbl_chat_rooms")
-@Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChatRoom extends BaseEntity {
-
+data class ChatRoom(
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "chat_room_id")
-	private Long id;
+	val id: Long? = null,
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	private Group group;
+	val group: Group
+) : BaseEntity() {
 
-	public Group getGroup() {
-		return group;
-	}
-
-	public Long getId() {
-		return id;
-	}
+	constructor(group: Group) : this(id = null, group = group)
 
 	/**
 	 * 채팅방 삭제
 	 */
-	public void delete() {
-		if (!this.getDisabled()) {
-			deactivate();
+	fun delete() {
+		if (!this.disabled) {
+			deactivate()
 		}
 	}
 }
