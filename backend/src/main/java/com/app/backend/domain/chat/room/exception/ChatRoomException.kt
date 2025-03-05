33a9e23c8b6afe2a1da648650b35f663e826df03
vdvhk12 <vdvhk12@gmail.com>
2025-03-05 10:03:31 +0900

    refactor: exception 패키지 코틀린으로 변환 (#11)

--- backend/src/main/java/com/app/backend/domain/chat/room/exception/ChatRoomException.kt
+++ backend/src/main/java/com/app/backend/domain/chat/room/exception/ChatRoomException.kt
@@ -1,10 +1,6 @@
-package com.app.backend.domain.chat.room.exception;
+package com.app.backend.domain.chat.room.exception
 
-import com.app.backend.global.error.exception.DomainErrorCode;
-import com.app.backend.global.error.exception.DomainException;
+import com.app.backend.global.error.exception.DomainErrorCode
+import com.app.backend.global.error.exception.DomainException
 
-public class ChatRoomException extends DomainException {
-	public ChatRoomException(final DomainErrorCode domainErrorCode) {
-		super(domainErrorCode);
-	}
-}
+class ChatRoomException(domainErrorCode: DomainErrorCode) : DomainException(domainErrorCode)
