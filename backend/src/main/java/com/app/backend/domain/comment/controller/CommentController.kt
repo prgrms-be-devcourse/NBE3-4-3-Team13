4f3a50222e8c2e9352b7fb050b8dc0f0666e3486
baekminjin <bhs3392@naver.com>
2025-03-05 15:22:20 +0900

    refactor: CommentController.java 코틀린으로 전환

--- backend/src/main/java/com/app/backend/domain/comment/controller/CommentController.kt
+++ backend/src/main/java/com/app/backend/domain/comment/controller/CommentController.kt
@@ -1,86 +1,76 @@
-package com.app.backend.domain.comment.controller;
+package com.app.backend.domain.comment.controller
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.app.backend.domain.comment.dto.request.CommentCreateRequest;
-import com.app.backend.domain.comment.dto.response.CommentResponse;
-import com.app.backend.domain.comment.service.CommentService;
-import com.app.backend.domain.member.entity.MemberDetails;
-import com.app.backend.global.annotation.CustomPageJsonSerializer;
-import com.app.backend.global.dto.response.ApiResponse;
-
-import lombok.RequiredArgsConstructor;
+import com.app.backend.domain.comment.dto.request.CommentCreateRequest
+import com.app.backend.domain.comment.dto.response.CommentResponse.*
+import com.app.backend.domain.comment.service.CommentService
+import com.app.backend.domain.member.entity.MemberDetails
+import com.app.backend.global.annotation.CustomPageJsonSerializer
+import com.app.backend.global.dto.response.ApiResponse
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1/comment")
-public class CommentController {
-
-	private final CommentService commentService;
-
+class CommentController(
+	private val commentService: CommentService
+) {
 	// 게시물에 대한 댓글 작성
 	@PostMapping("/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
-	public ApiResponse<CommentResponse.CommentDto> createComment(@PathVariable(name = "id") Long postId,
-		@RequestBody CommentCreateRequest req,
-		@AuthenticationPrincipal MemberDetails memberDetails){
-
-		CommentResponse.CommentDto response = commentService.createComment(postId, memberDetails.getId(), req);
+	fun createComment(
+		@PathVariable(name = "id") postId: Long,
+		@RequestBody req: CommentCreateRequest,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<CommentDto> {
+		val response = commentService.createComment(postId, memberDetails.id!!, req)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.CREATED,
-			"%d번 댓글이 작성되었습니다.".formatted(response.getId()),
+			"${response.id}번 댓글이 작성되었습니다.",
 			response
-		);
+		)
 	}
 
-	//댓글 삭제
+	// 댓글 삭제
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public ApiResponse<Void> deleteComment(@PathVariable(name = "id") Long commentId,
-		@AuthenticationPrincipal MemberDetails memberDetails){
-
-		commentService.deleteComment(commentId, memberDetails.getId());
+	fun deleteComment(
+		@PathVariable(name = "id") commentId: Long,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<Void> {
+		commentService.deleteComment(commentId, memberDetails.id!!)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.NO_CONTENT,
-			"%d번 댓글이 삭제되었습니다.".formatted(commentId)
-		);
+			"${commentId}번 댓글이 삭제되었습니다."
+		)
 	}
 
-	//댓글 수정
+	// 댓글 수정
 	@PatchMapping("/{id}")
-	public ApiResponse<CommentResponse.CommentDto> updateComment(@PathVariable(name = "id") Long commentId,
-		@RequestBody CommentCreateRequest req,
-		@AuthenticationPrincipal MemberDetails memberDetails){
-
-		CommentResponse.CommentDto response = commentService.updateComment(commentId, memberDetails.getId(),req);
+	fun updateComment(
+		@PathVariable(name = "id") commentId: Long,
+		@RequestBody req: CommentCreateRequest,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<CommentDto> {
+		val response = commentService.updateComment(commentId, memberDetails.id!!, req)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.OK,
-			"%d번 댓글이 수정되었습니다.".formatted(commentId),
+			"${commentId}번 댓글이 수정되었습니다.",
 			response
-		);
+		)
 	}
 
-	//게시물에 대한 댓글 조회 페이징
+	// 게시물에 대한 댓글 조회 페이징
 	@GetMapping("/{id}")
 	@CustomPageJsonSerializer(
 		empty = false,
@@ -90,73 +80,73 @@ public class CommentController {
 		hasPrevious = false,
 		isFirst = false
 	)
-	public ApiResponse<Page<CommentResponse.CommentList>> getComments(
-		@PathVariable(name = "id") Long postId,
-		@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-		@AuthenticationPrincipal MemberDetails memberDetails
-	) {
-		Page<CommentResponse.CommentList> response = commentService.getComments(postId, memberDetails.getId(), pageable);
+	fun getComments(
+		@PathVariable(name = "id") postId: Long,
+		@PageableDefault(size = 10, sort = ["created_at"], direction = Sort.Direction.DESC) pageable: Pageable,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<Page<CommentList>> {
+		val response = commentService.getComments(postId, memberDetails.id!!, pageable)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.OK,
 			"댓글이 조회되었습니다.",
 			response
-		);
+		)
 	}
 
-	//대댓글 작성
+	// 대댓글 작성
 	@PostMapping("/{id}/reply")
 	@ResponseStatus(HttpStatus.CREATED)
-	public ApiResponse<CommentResponse.ReplyDto> createReply(
-		@PathVariable(name = "id") Long commentId,
-		@RequestBody CommentCreateRequest req,
-		@AuthenticationPrincipal MemberDetails memberDetails
-	) {
-		CommentResponse.ReplyDto response = commentService.createReply(commentId, memberDetails.getId(), req);
+	fun createReply(
+		@PathVariable(name = "id") commentId: Long,
+		@RequestBody req: CommentCreateRequest,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<ReplyDto> {
+		val response = commentService.createReply(commentId, memberDetails.id!!, req)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.CREATED,
-			"%d번 댓글에 대한 답글이 작성되었습니다.".formatted(commentId),
+			"${commentId}번 댓글에 대한 답글이 작성되었습니다.",
 			response
-		);
+		)
 	}
 
-	//대댓글 수정
+	// 대댓글 수정
 	@PatchMapping("/{id}/reply")
-	public ApiResponse<CommentResponse.ReplyDto> updateReply(
-		@PathVariable(name = "id") Long replyId,
-		@RequestBody CommentCreateRequest req,
-		@AuthenticationPrincipal MemberDetails memberDetails
-	) {
-		CommentResponse.ReplyDto response = commentService.updateReply(replyId, memberDetails.getId(), req);
+	fun updateReply(
+		@PathVariable(name = "id") replyId: Long,
+		@RequestBody req: CommentCreateRequest,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<ReplyDto> {
+		val response = commentService.updateReply(replyId, memberDetails.id!!, req)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.OK,
-			"%d번 답글이 수정되었습니다.".formatted(replyId),
+			"${replyId}번 답글이 수정되었습니다.",
 			response
-		);
+		)
 	}
 
-	//대댓글 삭제
+	// 대댓글 삭제
 	@DeleteMapping("/{id}/reply")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public ApiResponse<Void> deleteReply(
-		@PathVariable(name = "id") Long replyId,
-		@AuthenticationPrincipal MemberDetails memberDetails
-	) {
-		commentService.deleteReply(replyId, memberDetails.getId());
+	fun deleteReply(
+		@PathVariable(name = "id") replyId: Long,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<Void> {
+		commentService.deleteReply(replyId, memberDetails.id!!)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.NO_CONTENT,
-			"%d번 답글이 삭제되었습니다.".formatted(replyId)
-		);
+			"${replyId}번 답글이 삭제되었습니다."
+		)
 	}
 
-	//대댓글 조회
+	// 대댓글 조회
 	@GetMapping("/{id}/reply")
 	@CustomPageJsonSerializer(
 		empty = false,
@@ -167,33 +157,32 @@ public class CommentController {
 		hasPrevious = false,
 		isFirst = false
 	)
-	public ApiResponse<Page<CommentResponse.ReplyList>> getReplies(
-		@PathVariable(name = "id") Long commentId,
-		@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
-	) {
-		Page<CommentResponse.ReplyList> response = commentService.getReplies(commentId, pageable);
+	fun getReplies(
+		@PathVariable(name = "id") commentId: Long,
+		@PageableDefault(size = 10, sort = ["created_at"], direction = Sort.Direction.DESC) pageable: Pageable
+	): ApiResponse<Page<ReplyList>> {
+		val response = commentService.getReplies(commentId, pageable)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.OK,
 			"답글이 조회되었습니다.",
 			response
-		);
+		)
 	}
 
-	//댓글 좋아요
+	// 댓글 좋아요
 	@PostMapping("/{id}/like")
-	public ApiResponse<Void> createCommentLike(@PathVariable(name = "id") Long commentId,
-		@AuthenticationPrincipal MemberDetails memberDetails) {
-
-		commentService.CommentLike(commentId, memberDetails.getId());
+	fun createCommentLike(
+		@PathVariable(name = "id") commentId: Long,
+		@AuthenticationPrincipal memberDetails: MemberDetails
+	): ApiResponse<Void> {
+		commentService.commentLike(commentId, memberDetails.id!!)
 
 		return ApiResponse.of(
 			true,
 			HttpStatus.OK,
 			"댓글 좋아요가 추가되었습니다."
-		);
+		)
 	}
-
-
 }
