9254fc3d0981f88f886654eea0eda9aeab27a21c
baekminjin <bhs3392@naver.com>
2025-03-05 15:20:08 +0900

    refactor: Comment DTO 코틀린으로 전환

--- backend/src/main/java/com/app/backend/domain/comment/dto/request/CommentCreateRequest.kt
+++ backend/src/main/java/com/app/backend/domain/comment/dto/request/CommentCreateRequest.kt
@@ -1,18 +1,7 @@
-package com.app.backend.domain.comment.dto.request;
+package com.app.backend.domain.comment.dto.request
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class CommentCreateRequest {
-
-	private String content;
-}
+data class CommentCreateRequest(
+    var content: String? = null
+)
 
 
