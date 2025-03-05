3bde47e5ea82790a98cc806292ce5fcb210f2f76
Honam Kim <tigerrla@naver.com>
2025-03-05 09:40:32 +0900

    refactor: 카카오 DTO + 사용하는 클래스 수정

--- backend/src/main/java/com/app/backend/domain/member/dto/kakao/KakaoUserInfo.java
+++
@@ -1,6 +0,0 @@
-package com.app.backend.domain.member.dto.kakao;
-
-public record KakaoUserInfo(
-	String id,
-	String nickname
-) {}
--- backend/src/main/java/com/app/backend/domain/member/dto/kakao/TokenDto.java
+++
@@ -1,8 +0,0 @@
-package com.app.backend.domain.member.dto.kakao;
-
-public record TokenDto(
-	String id,
-	String accessToken,
-	String refreshToken,
-	String role
-) {} 
