a349b58851f02fe073050b7bcd18c45c07f68497
vdvhk12 <vdvhk12@gmail.com>
2025-03-05 14:48:48 +0900

    refactor: ChatMessageConsumer.java 코틀린으로 전환

--- backend/src/main/java/com/app/backend/global/rabbitmq/ChatMessageConsumer.kt
+++ backend/src/main/java/com/app/backend/global/rabbitmq/ChatMessageConsumer.kt
@@ -1,28 +1,24 @@
-package com.app.backend.global.rabbitmq;
+package com.app.backend.global.rabbitmq
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Service;
-
-import com.app.backend.domain.chat.message.dto.response.MessageResponse;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.stereotype.Service
+import com.app.backend.domain.chat.message.dto.response.MessageResponse
+import io.github.oshai.kotlinlogging.KotlinLogging
 
 @Service
-@RequiredArgsConstructor
-@Slf4j
-public class ChatMessageConsumer {
+class ChatMessageConsumer(
+	private val messagingTemplate: SimpMessagingTemplate
+) {
+	private val log = KotlinLogging.logger {}
 
-	private final SimpMessagingTemplate messagingTemplate;
-
-	@RabbitListener(queues = "${rabbitmq.queue.name}")
-	public void onMessage(MessageResponse messageResponse) { // Queue에서 message를 구독
+	@RabbitListener(queues = ["\${rabbitmq.queue.name}"])
+	fun onMessage(messageResponse: MessageResponse) { // Queue에서 message를 구독
 		try {
-			log.info("Received message: {}", messageResponse);
-			messagingTemplate.convertAndSend("/exchange/chat.exchange/chat." + messageResponse.getChatRoomId(), messageResponse);
-		} catch (Exception e) {
-			log.error("Error processing message: " + e.getMessage());
+			log.info { "Received message: $messageResponse" }
+			messagingTemplate.convertAndSend("/exchange/chat.exchange/chat.${messageResponse.chatRoomId}", messageResponse)
+		} catch (e: Exception) {
+			log.error { "Error processing message: ${e.message}" }
 		}
 	}
 }
