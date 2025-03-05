7595403905afe00de9fb040c9d4f818e6cf223ac
vdvhk12 <vdvhk12@gmail.com>
2025-03-05 09:52:37 +0900

    refactor: ChatRoomDetailResponse 코틀린으로 변환 (#11)

--- backend/src/main/java/com/app/backend/domain/chat/room/dto/response/ChatRoomDetailResponse.kt
+++ backend/src/main/java/com/app/backend/domain/chat/room/dto/response/ChatRoomDetailResponse.kt
@@ -1,28 +1,16 @@
-package com.app.backend.domain.chat.room.dto.response;
+package com.app.backend.domain.chat.room.dto.response
 
-import java.util.List;
+import com.app.backend.domain.group.dto.response.GroupChatResponse
+import com.app.backend.domain.member.dto.response.MemberChatResponseDto
 
-import com.app.backend.domain.group.dto.response.GroupChatResponse;
-import com.app.backend.domain.member.dto.response.MemberChatResponseDto;
+data class ChatRoomDetailResponse(
+	val chatRoomId: Long,
+	val group: GroupChatResponse,
+) {
+	var members: List<MemberChatResponseDto> = emptyList()
+		private set
 
-import lombok.Getter;
-
-@Getter
-public class ChatRoomDetailResponse {
-
-	private final Long chatRoomId;
-
-	private final GroupChatResponse group;
-
-	private List<MemberChatResponseDto> members;
-
-	// QueryDSL에서 사용하는 생성자 추가
-	public ChatRoomDetailResponse(Long chatRoomId, GroupChatResponse group) {
-		this.chatRoomId = chatRoomId;
-		this.group = group;
-	}
-
-	public void addMembers(List<MemberChatResponseDto> members) {
-		this.members = members;
+	fun addMembers(members: List<MemberChatResponseDto>) {
+		this.members = members
 	}
 }
