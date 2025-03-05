f8ac389fedb97ce8e329fe57b3702c54ae51817b
vdvhk12 <vdvhk12@gmail.com>
2025-03-05 11:13:46 +0900

    refactor: ChatRoomRepository 엔티티 코틀린으로 변환 (#11)

--- backend/src/main/java/com/app/backend/domain/chat/room/repository/ChatRoomRepository.kt
+++ backend/src/main/java/com/app/backend/domain/chat/room/repository/ChatRoomRepository.kt
@@ -1,9 +1,6 @@
-package com.app.backend.domain.chat.room.repository;
+package com.app.backend.domain.chat.room.repository
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.app.backend.domain.chat.room.entity.ChatRoom
+import org.springframework.data.jpa.repository.JpaRepository
 
-import com.app.backend.domain.chat.room.entity.ChatRoom;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomRepositoryCustom {
-
-}
+interface ChatRoomRepository : JpaRepository<ChatRoom, Long>, ChatRoomRepositoryCustom
