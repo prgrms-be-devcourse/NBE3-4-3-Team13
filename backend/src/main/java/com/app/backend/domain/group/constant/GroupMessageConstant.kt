87357d59d4780e94e98977b7d07df3fdf0b764aa
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-05 23:04:15 +0900

    refactor: Group 관련 메시지 상수 코틀린 변환

--- backend/src/main/java/com/app/backend/domain/group/constant/GroupMessageConstant.kt
+++ backend/src/main/java/com/app/backend/domain/group/constant/GroupMessageConstant.kt
@@ -1,13 +1,13 @@
-package com.app.backend.domain.group.constant;
+package com.app.backend.domain.group.constant
 
-public abstract class GroupMessageConstant {
-    public static final String CREATE_GROUP_SUCCESS      = "모임이 성공적으로 생성되었습니다.";
-    public static final String READ_GROUP_SUCCESS        = "모임이 성공적으로 조회되었습니다.";
-    public static final String READ_GROUPS_SUCCESS       = "모임 목록이 성공적으로 조회되었습니다.";
-    public static final String UPDATE_GROUP_SUCCESS      = "모임이 성공적으로 수정되었습니다.";
-    public static final String DELETE_GROUP_SUCCESS      = "모임이 성공적으로 삭제되었습니다.";
-    public static final String LEAVE_GROUP_SUCCESS       = "모임에서 성공적으로 탈퇴했습니다.";
-    public static final String APPROVE_JOINING_SUCCESS   = "모임 신청을 승인했습니다.";
-    public static final String REJECT_JOINING_SUCCESS    = "모임 신청을 거절했습니다.";
-    public static final String MODIFY_GROUP_ROLE_SUCCESS = "모임 내 회원 권한이 성공적으로 변경되었습니다.";
+object GroupMessageConstant {
+    const val CREATE_GROUP_SUCCESS = "모임이 성공적으로 생성되었습니다."
+    const val READ_GROUP_SUCCESS = "모임이 성공적으로 조회되었습니다."
+    const val READ_GROUPS_SUCCESS = "모임 목록이 성공적으로 조회되었습니다."
+    const val UPDATE_GROUP_SUCCESS = "모임이 성공적으로 수정되었습니다."
+    const val DELETE_GROUP_SUCCESS = "모임이 성공적으로 삭제되었습니다."
+    const val LEAVE_GROUP_SUCCESS = "모임에서 성공적으로 탈퇴했습니다."
+    const val APPROVE_JOINING_SUCCESS = "모임 신청을 승인했습니다."
+    const val REJECT_JOINING_SUCCESS = "모임 신청을 거절했습니다."
+    const val MODIFY_GROUP_ROLE_SUCCESS = "모임 내 회원 권한이 성공적으로 변경되었습니다."
 }
