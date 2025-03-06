e696c710de1e1943844febfae346910f7eb9ccdb
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-06 14:33:42 +0900

    refactor: AopConfig 코틀린 변환

--- backend/src/main/java/com/app/backend/global/config/AopConfig.kt
+++ backend/src/main/java/com/app/backend/global/config/AopConfig.kt
@@ -1,22 +1,16 @@
-package com.app.backend.global.config;
+package com.app.backend.global.config
 
-import com.app.backend.global.aop.AppAspect.LockAspect;
-import com.app.backend.global.aop.AppAspect.PageJsonSerializerAspect;
-import com.app.backend.global.util.LockManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.app.backend.global.aop.AppAspect.Companion.LockAspect
+import com.app.backend.global.aop.AppAspect.Companion.PageJsonSerializerAspect
+import com.app.backend.global.util.LockManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-public class AopConfig {
+class AopConfig {
+    @Bean
+    fun pageJsonSerializerAspect() = PageJsonSerializerAspect()
 
     @Bean
-    public PageJsonSerializerAspect pageJsonSerializerAspect() {
-        return new PageJsonSerializerAspect();
-    }
-
-    @Bean
-    public LockAspect redissonLockAspect(final LockManager lockManager) {
-        return new LockAspect(lockManager);
-    }
-
+    fun redissonLockAspect(lockManager: LockManager) = LockAspect(lockManager)
 }
