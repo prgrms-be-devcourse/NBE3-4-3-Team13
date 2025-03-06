8e631074cace7a84565c91fc27c39e5e14289bd2
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-06 12:19:35 +0900

    refactor: AppUtil 코틀린 변환

--- backend/src/main/java/com/app/backend/global/util/AppUtil.kt
+++ backend/src/main/java/com/app/backend/global/util/AppUtil.kt
@@ -1,23 +1,14 @@
-package com.app.backend.global.util;
+package com.app.backend.global.util
 
-import jakarta.validation.constraints.NotNull;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-public class AppUtil {
+object AppUtil {
+    private val DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    private val DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private static final DateFormat        DATE_FORMAT         = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-    public static String localDateTimeToString(@NotNull final LocalDateTime localDateTime) {
-        return DATE_TIME_FORMATTER.format(localDateTime);
-    }
-
-    public static String DateToString(@NotNull final Date date) {
-        return DATE_FORMAT.format(date);
-    }
-
+    fun localDateTimeToString(localDateTime: LocalDateTime) = DATE_TIME_FORMATTER.format(localDateTime)
+    fun DateToString(date: Date) = DATE_FORMAT.format(date)
 }
