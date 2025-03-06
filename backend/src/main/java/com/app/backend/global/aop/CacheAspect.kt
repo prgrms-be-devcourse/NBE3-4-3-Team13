fcd2fb09eec83b62f1ce017f2483e3fffa2b8556
vdvhk12 <vdvhk12@gmail.com>
2025-03-06 10:38:38 +0900

    refactor: CacheAspect.java 코틀린 전환

--- backend/src/main/java/com/app/backend/global/aop/CacheAspect.kt
+++ backend/src/main/java/com/app/backend/global/aop/CacheAspect.kt
@@ -1,125 +1,102 @@
-package com.app.backend.global.aop;
+package com.app.backend.global.aop
 
-import com.app.backend.domain.member.entity.MemberDetails;
-import com.app.backend.global.annotation.CustomCache;
-import com.app.backend.global.annotation.CustomCacheDelete;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
+import com.app.backend.domain.member.entity.MemberDetails
+import com.app.backend.global.annotation.CustomCache
+import com.app.backend.global.annotation.CustomCacheDelete
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.reflect.MethodSignature
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Slf4j
 @Aspect
 @Component
-@RequiredArgsConstructor
-public class CacheAspect {
+class CacheAspect(private val redisTemplate: RedisTemplate<String, Any>) {
 
-    private static final String UPDATE_KEY = "update";
-    private static final String HISTORY_KEY = "history";
-    private static final String VIEW_COUNT_PREFIX = "viewCount";
-    private final RedisTemplate<String, Object> redisTemplate;
+    companion object {
+        private const val UPDATE_KEY = "update"
+        private const val HISTORY_KEY = "history"
+        private const val VIEW_COUNT_PREFIX = "viewCount"
+    }
 
     @Around("@annotation(customCache)")
-    public Object aroundG(ProceedingJoinPoint joinPoint, CustomCache customCache) throws Throwable {
-        String cacheKey = generateKey(customCache.prefix(), customCache.key(), customCache.id(), getParams(joinPoint));
-        String viewCountKey = VIEW_COUNT_PREFIX + ":" + cacheKey;
-        String limitUserKey = cacheKey + ":user:" + getUserID();
-        String updateKeyList = customCache.prefix() + ":" + UPDATE_KEY;
-        String historyKey = customCache.prefix() + ":" + HISTORY_KEY;
+    @Throws(Throwable::class)
+    fun aroundG(joinPoint: ProceedingJoinPoint, customCache: CustomCache): Any? {
+        val cacheKey = generateKey(customCache.prefix, customCache.key, customCache.id, getParams(joinPoint))
+        val viewCountKey = "$VIEW_COUNT_PREFIX:$cacheKey"
+        val limitUserKey = "$cacheKey:user:${getUserID()}"
+        val updateKeyList = "${customCache.prefix}:$UPDATE_KEY"
+        val historyKey = "${customCache.prefix}:$HISTORY_KEY"
 
-        Object result = null;
-
-        try {
-            // 조회수
-            if (customCache.viewCount() && !redisTemplate.hasKey(limitUserKey)) {
-                redisTemplate.opsForValue().increment(viewCountKey);
-                redisTemplate.opsForValue().set(limitUserKey, true, customCache.viewCountTtl(), customCache.viewCountTtlUnit());
-
-                redisTemplate.opsForSet().add(updateKeyList, viewCountKey);
+        return try {
+            // 조회수 증가
+            if (customCache.viewCount && !redisTemplate.hasKey(limitUserKey)) {
+                redisTemplate.opsForValue().increment(viewCountKey)
+                redisTemplate.opsForValue().set(limitUserKey, true, customCache.viewCountTtl, customCache.viewCountTtlUnit)
+                redisTemplate.opsForSet().add(updateKeyList, viewCountKey)
             }
 
-            Object cachedData = redisTemplate.opsForValue().get(cacheKey);
+            val cachedData = redisTemplate.opsForValue().get(cacheKey)
 
-            // 조회 기록
-            if (customCache.history()) {
-                redisTemplate.opsForSet().add(historyKey, cacheKey);
+            // 조회 기록 저장
+            if (customCache.history) {
+                redisTemplate.opsForSet().add(historyKey, cacheKey)
             }
 
-            if (cachedData != null) {
-                return cachedData;
+            cachedData ?: joinPoint.proceed().also {
+                redisTemplate.opsForValue().set(cacheKey, it, customCache.ttl, customCache.ttlUnit)
             }
-
-            result = joinPoint.proceed();
-
-            redisTemplate.opsForValue().set(cacheKey, result, customCache.ttl(), customCache.ttlUnit());
-
-            return result;
-        } catch (Exception e) {
-            return joinPoint.proceed();
+        } catch (e: Exception) {
+            joinPoint.proceed()
         }
     }
 
     @Around("@annotation(customCacheDelete)")
-    public Object aroundD(ProceedingJoinPoint joinPoint, CustomCacheDelete customCacheDelete) throws Throwable {
-        String cacheKey = generateKey(customCacheDelete.prefix(), customCacheDelete.key(), customCacheDelete.id(), getParams(joinPoint));
+    @Throws(Throwable::class)
+    fun aroundD(joinPoint: ProceedingJoinPoint, customCacheDelete: CustomCacheDelete): Any? {
+        val cacheKey = generateKey(customCacheDelete.prefix, customCacheDelete.key, customCacheDelete.id, getParams(joinPoint))
 
-        try {
-
+        return try {
             if (redisTemplate.hasKey(cacheKey)) {
-                redisTemplate.delete(cacheKey);
+                redisTemplate.delete(cacheKey)
             }
-
-            return joinPoint.proceed();
-        } catch (Exception e) {
-            return joinPoint.proceed();
+            joinPoint.proceed()
+        } catch (e: Exception) {
+            joinPoint.proceed()
         }
     }
 
-    private Long getUserID() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
-        return memberDetails.getId();
+    private fun getUserID(): Long {
+        val authentication: Authentication = SecurityContextHolder.getContext().authentication
+        val memberDetails = authentication.principal as MemberDetails
+        return memberDetails.id
     }
 
-    private Map<String, Object> getParams(ProceedingJoinPoint joinPoint) {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        String[] paramsName = methodSignature.getParameterNames();
-        Object[] args = joinPoint.getArgs();
+    private fun getParams(joinPoint: ProceedingJoinPoint): Map<String, Any> {
+        val methodSignature = joinPoint.signature as MethodSignature
+        val paramsName = methodSignature.parameterNames
+        val args = joinPoint.args
 
-        Map<String, Object> params = new HashMap<>();
-
-        for (int i = 0; i < paramsName.length; i++) {
-            params.put(paramsName[i], args[i]);
-        }
-
-        return params;
+        return paramsName.zip(args).toMap()
     }
 
-    private String generateKey(String prefix, String key, String id, Map<String, Object> params) {
-        StringBuilder newKey = new StringBuilder(prefix);
+    private fun generateKey(prefix: String, key: String, id: String, params: Map<String, Any>): String {
+        val newKey = StringBuilder(prefix)
 
-        if (!key.isEmpty()) {
-            newKey.append(":").append(key);
+        if (key.isNotEmpty()) {
+            newKey.append(":").append(key)
         }
 
-        if (!id.isEmpty() && params.containsKey(id)) {
-            newKey.append(":").append(params.get(id));
-            return newKey.toString();
+        if (id.isNotEmpty() && params.containsKey(id)) {
+            newKey.append(":").append(params[id])
+            return newKey.toString()
         }
 
-        for (Map.Entry<String, Object> entry : params.entrySet()) {
-            newKey.append(":").append(entry.getValue().toString());
-        }
+        params.values.forEach { newKey.append(":").append(it) }
 
-        return newKey.toString();
+        return newKey.toString()
     }
 }
