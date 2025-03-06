2bdc357254989c08cbb2fe5c069f2622e2719012
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-06 10:33:26 +0900

    refactor: GroupLikeRepository 코틀린 변환

--- backend/src/main/java/com/app/backend/domain/group/repository/GroupLikeRepository.kt
+++ backend/src/main/java/com/app/backend/domain/group/repository/GroupLikeRepository.kt
@@ -1,20 +1,18 @@
-package com.app.backend.domain.group.repository;
+package com.app.backend.domain.group.repository
 
-import com.app.backend.domain.group.entity.Group;
-import com.app.backend.domain.group.entity.GroupLike;
-import com.app.backend.domain.member.entity.Member;
-import jakarta.persistence.LockModeType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
+import com.app.backend.domain.group.entity.Group
+import com.app.backend.domain.group.entity.GroupLike
+import com.app.backend.domain.member.entity.Member
+import jakarta.persistence.LockModeType
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
-import java.util.Optional;
-
-public interface GroupLikeRepository extends JpaRepository<GroupLike, Long> {
-
-    long countByGroupIdAndMemberId(Long groupId, Long memberId);
+interface GroupLikeRepository : JpaRepository<GroupLike, Long>, GroupLikeRepositoryCustom {
+    fun countByGroupIdAndMemberId(groupId: Long, memberId: Long): Long
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT gl FROM GroupLike gl WHERE gl.group = :group AND gl.member = :member")
-    Optional<GroupLike> findByGroupAndMember(Group group, Member member);
+    fun findByGroupAndMember(group: Group, member: Member): Optional<GroupLike>
 }
