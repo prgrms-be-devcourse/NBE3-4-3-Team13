0cf615f57b38c231ae625341a2b6e733d8e9fca1
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-10 11:45:59 +0900

    feat: LockHelper 추가 등
    - LockHelper: AOP 방식이 아닌 특정 로직에만 적용하도록 만든 컴포넌트
    - LockManager: LockHelper와 공통으로 사용하는 로직 분리
    - LockUtil: LockManager/LockHelper에서 공통으로 사용하는 로직(재시도 등)

---
+++ backend/src/main/kotlin/com/app/backend/global/util/LockHelper.kt
@@ -0,0 +1,27 @@
+package com.app.backend.global.util
+
+import org.redisson.api.RedissonClient
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+class LockHelper(private val redissonClient: RedissonClient, private val lockUtil: LockUtil) {
+    fun <R> executeWithLock(
+        lockKey: String,
+        maxWaitTime: Long,
+        leaseTime: Long,
+        timeUnit: TimeUnit = TimeUnit.MILLISECONDS,
+        block: () -> R
+    ): R {
+        val lock = redissonClient.getLock(lockKey)
+        return if (lockUtil.lockWithRetry(lock, timeUnit.toMillis(maxWaitTime), timeUnit.toMillis(leaseTime)))
+            try {
+                block()
+            } finally {
+                if (lock.isHeldByCurrentThread)
+                    lock.unlock()
+            }
+        else
+            throw RuntimeException("Failed to acquire lock: $lockKey")
+    }
+}
