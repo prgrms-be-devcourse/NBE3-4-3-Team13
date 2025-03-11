d655b09e6c4ea236f87ac578fbd90bb8418b8a81
vdvhk12 <vdvhk12@gmail.com>
2025-03-11 16:31:02 +0900

    feat: signedUrl 반환하는 컨트롤러 추가
    - 이렇게 해도 CORS 설정을 따로 해줘야함.
    - Google Cloud SDK 설치 후, gsutil cors set cors.json gs://my-bucket-name 명령어로 설정 해줌.

---
+++ backend/src/main/kotlin/com/app/backend/domain/attachment/controller/FileUploadController.java
@@ -0,0 +1,64 @@
+package com.app.backend.domain.attachment.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.HttpMethod;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class FileUploadController {
+
+	private final Storage storage;
+	private final String bucketName = "bucket-get-started_linkus-453401"; // 구글 클라우드 스토리지 버킷 이름
+
+	@GetMapping("/generate-signed-url")
+	public ResponseEntity<String> generateSignedUrl(@RequestParam String filename) {
+		// 스토리지에서 파일 찾기
+		Blob blob = storage.get(bucketName, filename);
+
+		// 파일이 존재하면 에러 반환
+		if (blob != null) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 존재하는 파일입니다.");
+		}
+
+		// 새 파일을 업로드할 수 있도록 Signed URL 생성
+		BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, filename).build();
+		URL signedUrl = storage.signUrl(
+			blobInfo,
+			15, TimeUnit.MINUTES, // URL 유효 기간
+			Storage.SignUrlOption.withV4Signature(), // V4 서명 사용
+			Storage.SignUrlOption.httpMethod(HttpMethod.PUT) // 파일 업로드용 URL
+		);
+
+		return ResponseEntity.ok(signedUrl.toString()); // 생성된 Signed URL 반환
+	}
+
+	private FileUploadController() throws IOException {
+		// resources에서 서비스 계정 키 로드
+		ClassPathResource resource = new ClassPathResource("linkus-453401.json");
+		InputStream serviceAccountStream = resource.getInputStream();
+
+		// 인증 정보로 GCS 클라이언트 생성
+		GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccountStream);
+		this.storage = StorageOptions.newBuilder()
+			.setCredentials(credentials)
+			.build()
+			.getService();
+	}
+}
