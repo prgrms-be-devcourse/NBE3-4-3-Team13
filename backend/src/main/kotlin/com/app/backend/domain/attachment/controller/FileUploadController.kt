8dfa1f080f25db847533397cdcbe01ed09366b19
vdvhk12 <vdvhk12@gmail.com>
2025-03-12 15:39:11 +0900

    refactor: 구글 클라우드 스토리지 연결 방법 수정
    - StorageOptions.getDefaultInstance().service 를 사용해서 자동으로 키 파일 주입하도록 수정

--- backend/src/main/kotlin/com/app/backend/domain/attachment/controller/FileUploadController.kt
+++ backend/src/main/kotlin/com/app/backend/domain/attachment/controller/FileUploadController.kt
@@ -1,19 +1,16 @@
 package com.app.backend.domain.attachment.controller
 
-import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.HttpMethod
 import com.google.cloud.storage.Storage
 import com.google.cloud.storage.StorageOptions
-import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.io.InputStream
 import java.net.URL
 import java.util.concurrent.TimeUnit
 
@@ -21,21 +18,8 @@ import java.util.concurrent.TimeUnit
 @RequestMapping("/api/v1/")
 class FileUploadController {
 
-	private lateinit var storage: Storage
+	private val storage: Storage = StorageOptions.getDefaultInstance().service
 	private val bucketName = "bucket-get-started_linkus-453401" // 구글 클라우드 스토리지 버킷 이름
-
-	init {
-		// resources에서 서비스 계정 키 로드
-		val resource = ClassPathResource("linkus-453401.json")
-		val serviceAccountStream: InputStream = resource.inputStream
-
-		// 인증 정보로 GCS 클라이언트 생성
-		val credentials = GoogleCredentials.fromStream(serviceAccountStream)
-		storage = StorageOptions.newBuilder()
-			.setCredentials(credentials)
-			.build()
-			.service
-	}
 
 	@GetMapping("/generate-signed-url")
 	fun generateSignedUrl(@RequestParam filename: String): ResponseEntity<String> {
